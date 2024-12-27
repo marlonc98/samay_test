2eb109c8cbad2d5439e5d5c466ec4ee4c364266a
Marlon <mackiller1998@gmail.com>
2024-12-27 14:48:44 -0500

    changed image

--- lib/utils/images_constants.dart
+++ lib/utils/images_constants.dart
@@ -1,7 +1,7 @@
 class ImagesConstants {
   static const String splashNotFound = 'assets/animations/not_found.json';
   static const String splashLoading = 'assets/animations/splash.json';
-  static const String imageNotFound = 'assets/images/cat_not_found.png';
+  static const String imageNotFound = 'assets/images/project_not_found.png';
   static const String logo = 'assets/images/logo/logo.svg';
   static const String logoWhite = 'assets/images/logo/logo_white.svg';
   static const String bgProjectsPage = 'assets/images/bg_projects_page.jpg';
